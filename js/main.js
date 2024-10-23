9d542ec92a5c454c57109b2416a986dea4de4218
vlacheslav katerynych <bvelorant@gmail.com>
2024-10-23 11:58:00 +0300

    labs__html

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const MenuBtn = document.querySelector('.burger__btn');
-const MenuNav = document.querySelector('.nav__menu');
-
-MenuBtn.addEventListener('click', ()=> {
-   MenuNav.classList.toggle('nav__menu--active')
-})
